f65500b9805c8b6951c0cc4e3888c2d77f28c278
马泽朋 <laoma17@outlook.com>
2020-01-15 15:07:36 +0800

    测试

---
+++ src/main/java/co/laomag/immigration/ImmigrationApplication.java
@@ -0,0 +1,13 @@
+package co.laomag.immigration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImmigrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImmigrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/laomag/immigration/ImmigrationApplicationTests.java
@@ -0,0 +1,16 @@
+package co.laomag.immigration;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ImmigrationApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
